7bb1710c0d444e8541c662ea5e01a47ee0100da5
Miriam Olivier <miriamolivier098@gmail.com>
2022-11-30 15:35:00 +0200

    Auto close navbar onclick javascript

--- script.js
+++ script.js
@@ -1,24 +1,8 @@
-const navSlide = () => {
-  const burger = document.querySelector(".burger");
-  const nav = document.querySelector(".menu");
-  const navLinks = document.querySelectorAll(".menu li");
 
-  burger.addEventListener("click", () => {
-    //Toggle Nav
-    nav.classList.toggle("nav-active");
+const navLinkClick = () => {
+  document.getElementById("toggleNav").addEventListener("click", () => {
+    document.getElementById('toggle').checked = false;
+  })
+}
 
-    //Animate Nav
-    navLinks.forEach((link, index) => {
-      if (link.style.animation) {
-        link.style.animation = "";
-      } else {
-        link.style.animation = `navLinkFade 0.5s ease forwards
-         ${index / 5 + 0.3}s`;
-      }
-    });
-
-    //Burger animation
-    burger.classList.toggle("toggle");
-  });
-};
-navSlide();
+navLinkClick();
