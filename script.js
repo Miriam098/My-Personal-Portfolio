4199c88e6b3335f7e2a0698abdfd63ddee9b8c9c
Miriam Olivier <miriamolivier098@gmail.com>
2022-11-30 17:18:19 +0200

    read more button added to about page

--- script.js
+++ script.js
@@ -6,3 +6,28 @@ const navLinkClick = () => {
 }
 
 navLinkClick();
+
+
+const readMore = () => {
+  let btn = document.getElementById('readMoreBtn');
+  let moreText = document.getElementById('text');
+  let dots = document.getElementById('dots');
+
+  btn.addEventListener('click', () => {
+    if (dots.style.display === 'none') {
+      dots.style.display = 'inline';
+      btn.innerHTML = 'Read More';
+      moreText.style.display = 'none';
+
+    } else {
+      dots.style.display = 'none';
+      btn.innerHTML = 'Read Less';
+      moreText.style.display = 'inline'
+    }
+  })
+}
+
+
+readMore();
+
+console.log()
